1df1ae0181bddcb61e7bd7f02a678dc58c21649f
koheiSugawara <59728817+koheiSugawara@users.noreply.github.com>
2021-08-06 16:33:42 +0900

    ライブラリ追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import List from './components/List';
 import New from './components/New';
 import Detail from './components/Detail';
 import Edit from './components/Edit';
+import Header from './components/commons/Header';
+import MainContainer from './components/layout/MainContainer';
+// style
+import { CssBaseline } from '@material-ui/core';
+import { StylesProvider, ThemeProvider } from '@material-ui/styles';
+import { theme } from './styles/theme';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={List} />
-        <Route path='/post/:id' component={Detail} />
-        <Route exact path='/new' component={New} />
-        <Route path='/edit/:id' component={Edit}/>
-      </Switch>
-    </Router>
+    <>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Header />
+          <MainContainer>
+            <Router>
+              <Switch>
+                <Route exact path='/' component={List} />
+                <Route path='/post/:id' component={Detail} />
+                <Route exact path='/new' component={New} />
+                <Route path='/edit/:id' component={Edit} />
+              </Switch>
+            </Router>
+          </MainContainer>
+        </ThemeProvider>
+      </StylesProvider>
+    </>
   );
 };
 export default App;
